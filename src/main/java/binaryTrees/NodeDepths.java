f8074fcccefa37326c0373d853b35abed76db0ea
ragothamang <ragothamanu@gmail.com>
2022-12-13 00:12:22 +0530

    Node depths - BinaryTree - Easy AlgoExpert

---
+++ src/main/java/binaryTrees/NodeDepths.java
@@ -0,0 +1,35 @@
+package binaryTrees;
+
+public class NodeDepths {
+	
+	private static int nodeDepths(BinaryTree root, int depth) {
+	    if (root == null) return 0;
+
+	    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+	  }
+
+	  public static int nodeDepths(BinaryTree root) {
+	    // Write your code here.
+	    if (root == null) return -1;
+
+	    return nodeDepths(root, 0);
+	  }
+
+	  static class BinaryTree {
+	    int value;
+	    BinaryTree left;
+	    BinaryTree right;
+
+	    public BinaryTree(int value) {
+	      this.value = value;
+	      left = null;
+	      right = null;
+	    }
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

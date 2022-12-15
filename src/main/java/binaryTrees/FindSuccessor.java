9295a389bf73497dbe81d486fff63b2b29bb6231
ragothamang <ragothamanu@gmail.com>
2022-12-15 23:00:39 +0530

    FindSuccsessor-BinaryTree-AlgoExpert -CleanUp

--- src/main/java/binaryTrees/FindSuccessor.java
+++ src/main/java/binaryTrees/FindSuccessor.java
@@ -1,43 +1,43 @@
 package binaryTrees;
 
 public class FindSuccessor {
-	 static class BinaryTree {
-		    public int value;
-		    public BinaryTree left = null;
-		    public BinaryTree right = null;
-		    public BinaryTree parent = null;
+	static class BinaryTree {
+		public int value;
+		public BinaryTree left = null;
+		public BinaryTree right = null;
+		public BinaryTree parent = null;
 
-		    public BinaryTree(int value) {
-		      this.value = value;
-		    }
-		  }
+		public BinaryTree(int value) {
+			this.value = value;
+		}
+	}
 
-		  private BinaryTree leftMostChild(BinaryTree tree) {
-		    BinaryTree root = tree;
+	private BinaryTree leftMostChild(BinaryTree tree) {
+		BinaryTree root = tree;
 
-		    while (root.left != null) root = root.left;
+		while (root.left != null) root = root.left;
 
-		    return root;
-		  }
+		return root;
+	}
 
-		  public BinaryTree findSuccessor(BinaryTree tree, BinaryTree node) {
-		    // Write your code here.
-		    if (node.right != null) return leftMostChild(node.right);
+	public BinaryTree findSuccessor(BinaryTree tree, BinaryTree node) {
+		// Write your code here.
+		if (node.right != null) return leftMostChild(node.right);
 
-		    BinaryTree currentNode = node;
-		    BinaryTree ancestor = node.parent;
+		BinaryTree currentNode = node;
+		BinaryTree ancestor = node.parent;
 
-		    // We keep traversing up the tree
-		    // until we've reached a point where
-		    // the current node is located in the
-		    // left subtree of a parent
-		    while (ancestor != null && ancestor.left != currentNode) {
-		      currentNode = ancestor;
-		      ancestor = ancestor.parent;
-		    }
+		// We keep traversing up the tree
+		// until we've reached a point where
+		// the current node is located in the
+		// left subtree of a parent
+		while (ancestor != null && ancestor.left != currentNode) {
+			currentNode = ancestor;
+			ancestor = ancestor.parent;
+		}
 
-		    return ancestor;
-		  }
+		return ancestor;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

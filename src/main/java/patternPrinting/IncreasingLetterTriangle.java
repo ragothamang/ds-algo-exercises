451810f3559ee43c4843c0d9095ad4c082621a1d
ragothaman <ragothaman@ragothaman-ThinkPad-T450>
2023-02-10 00:19:27 +0530

    Increasing Letter triangle - Pattern printing

---
+++ src/main/java/patternPrinting/IncreasingLetterTriangle.java
@@ -0,0 +1,18 @@
+package patternPrinting;
+
+public class IncreasingLetterTriangle {
+	public static void nLetterTriangle(int n) {
+        // Write your code here
+		for(int i=0;i<n;i++) {
+			for(char ch='A';ch <= 'A'+i;ch++) {
+				System.out.print(ch+" ");
+			}
+			System.out.println();
+		}
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nLetterTriangle(5);
+	}
+
+}

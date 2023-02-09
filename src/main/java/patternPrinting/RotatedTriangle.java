c4f3ac0fe20dc6cb8c770b793e148e66b1e380c8
ragothaman <ragothaman@ragothaman-ThinkPad-T450>
2023-02-09 22:20:43 +0530

    Rotated Triangle - Pattern Printing

---
+++ src/main/java/patternPrinting/RotatedTriangle.java
@@ -0,0 +1,21 @@
+package patternPrinting;
+
+public class RotatedTriangle {
+	public static void nStarTriangle(int n) {
+        // Write your code here
+		for(int i=1;i<=2*n-1;i++) {
+			int stars =i;
+			if(i>n) stars = 2*n-i;
+			for(int j=1;j<=stars;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nStarTriangle(5);
+	}
+
+}

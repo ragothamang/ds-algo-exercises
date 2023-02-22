20eeb7d56aa9bc505b23defb85c5cfb266b641a7
ragothamang <ragothamanu@gmail.com>
2023-02-23 00:13:40 +0530

    numberOfWaysToMakeChange - DP- AlogExpert - medium

---
+++ src/main/java/DynamicProgramming/NumberOfWaysToMakeChange.java
@@ -0,0 +1,29 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class NumberOfWaysToMakeChange {
+
+	public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+	    // Write your code here.
+	    int[] numberOfWays = new int[n + 1];
+	    Arrays.fill(numberOfWays, 0);
+	    numberOfWays[0] = 1;
+
+	    for (int denom : denoms) {
+	      for (int target = 1; target <= n; target++) {
+	        if (target >= denom) numberOfWays[target] += numberOfWays[target - denom];
+	      }
+	    }
+	    return numberOfWays[n];
+	}
+
+	    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 3, 1, 2 };
+		System.out.println(numberOfWaysToMakeChange(7, arr));
+
+	}
+
+}

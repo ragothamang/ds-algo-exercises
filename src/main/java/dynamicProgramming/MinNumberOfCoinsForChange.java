9615bd26ccb0023ef12bdbd320081efec7c136bd
ragothamang <ragothamanu@gmail.com>
2023-02-23 22:36:51 +0530

    minNumberOfCoinsForChange - AlgoExpert - DP - Medium

--- src/main/java/dynamicProgramming/MinNumberOfCoinsForChange.java
+++ src/main/java/dynamicProgramming/MinNumberOfCoinsForChange.java
@@ -4,34 +4,34 @@ import java.util.Arrays;
 
 public class MinNumberOfCoinsForChange {
 	public static int minNumberOfCoinsForChange(int n, int[] denoms) {
-	    // Write your code here.
-	    int[] minCoinsRequired = new int[n + 1];
-	    Arrays.fill(minCoinsRequired, Integer.MAX_VALUE);
+		// Write your code here.
+		int[] minCoinsRequired = new int[n + 1];
+		Arrays.fill(minCoinsRequired, Integer.MAX_VALUE);
 
-	    minCoinsRequired[0] = 0;
-	    int minCoins = 0;
+		minCoinsRequired[0] = 0;
+		int minCoins = 0;
 
-	    for (int denom : denoms) {
-	      for (int target = 0; target <= n; target++) {
-	        if (target < denom) continue;
+		for (int denom : denoms) {
+			for (int target = 0; target <= n; target++) {
+				if (target < denom) continue;
 
-	        minCoins =
-	            minCoinsRequired[target - denom] == Integer.MAX_VALUE
-	                ? Integer.MAX_VALUE
-	                : 1 + minCoinsRequired[target - denom];
+				minCoins =
+						minCoinsRequired[target - denom] == Integer.MAX_VALUE
+						? Integer.MAX_VALUE
+								: 1 + minCoinsRequired[target - denom];
 
-	        minCoinsRequired[target] = Math.min(minCoinsRequired[target], minCoins);
-	      }
-	    }
+				minCoinsRequired[target] = Math.min(minCoinsRequired[target], minCoins);
+			}
+		}
 
-	    return minCoinsRequired[n] != Integer.MAX_VALUE ? minCoinsRequired[n] : -1;
-	  }
+		return minCoinsRequired[n] != Integer.MAX_VALUE ? minCoinsRequired[n] : -1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] ip = {1,5,10};
 		System.out.println(minNumberOfCoinsForChange(7, ip));
-		
+
 	}
 
 }

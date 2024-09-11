72c9d30a30c84904275fbc2e8383a35c5a737a1b
ragothamang <ragothamanu@gmail.com>
2024-09-11 15:28:59 +0530

    abstract keyword learning

---
+++ src/main/java/abstractKeywordLearning/AbstractClassSample.java
@@ -0,0 +1,10 @@
+package abstractKeywordLearning;
+
+public abstract class AbstractClassSample {
+	public abstract void sound();
+    
+    // Regular method with implementation
+    public void sleep() {
+        System.out.println("This animal is sleeping.");
+    }
+}
---
+++ src/main/java/abstractKeywordLearning/SubClassForAbstractClass.java
@@ -0,0 +1,16 @@
+package abstractKeywordLearning;
+
+public class SubClassForAbstractClass extends AbstractClassSample{
+	public void sound() {
+        System.out.println("The dog barks.");
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractClassSample dog = new SubClassForAbstractClass();
+	        dog.sound();  // Outputs: The dog barks.
+	        dog.sleep();  // Outputs: This animal is sleepin
+
+	}
+
+}
